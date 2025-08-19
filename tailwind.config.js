a6ecfc650f3d40817080743033317fccd0e5c8fc
Avinash <avinashkmg1310@gmail.com>
2025-08-20 04:59:20 +0530

    css animation - div in circular rotataion with animated styles

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./*.html"], // Adjust as per your file structure
+	theme: {
+		extend: {}, // Add custom theme overrides if needed
+	},
+};
